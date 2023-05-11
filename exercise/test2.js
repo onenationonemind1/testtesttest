a16edff50a51f123d8b7f75f855b850fa6481781
onenationonemind1 <rkfmvk1117@gmail.com>
2023-05-12 02:03:13 +0900

    g

--- exercise/test2.js
+++ exercise/test2.js
@@ -4,15 +4,22 @@ const fs = require("fs");
 const configuration = new Configuration({
   apiKey: process.env.CHATGPT_API_KEY,
 });
+const openai = new OpenAIApi(configuration);
 
 async function transcribe(filename) {
-  const openai = new OpenAIApi(configuration);
-  const resp = await openai.createTranscription(
-    fs.createReadStream(filename),
-    "whisper-1" // Whisper model name.
-  );
-
-  console.log(resp.data.text);
+  try {
+    const resp = await openai.createTranscription(
+      fs.createReadStream(filename),
+      "whisper-1", //사용할 모델
+      "undefined", // the prompt to use for transcription
+      "json", // the format of the transcription
+      1, // temperture
+      "en"
+    );
+    console.log(resp.data.text);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 transcribe("b.mp3");
